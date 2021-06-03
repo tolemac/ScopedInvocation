1aa89c4e69b22eef1082a7f7eb407429fe4ff6c7
Javier Ros <jros@emuasa.es>
2021-06-03 13:58:44 +0200

    feat: add EFCore transaction manager using dbcontext transaction

---
+++ src/InvocationContext.EntityFrameworkCore/DbContextTransactionManager.cs
@@ -0,0 +1,32 @@
+﻿using InvocationContext.Transactional;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace InvocationContext.EntityFrameworkCore
+{
+    public class DbContextTransactionManager<TDbContext> : ITransactionManager
+        where TDbContext : DbContext
+    {
+        private readonly TDbContext _dbContext;
+        private IDbContextTransaction? _trans;
+
+        public DbContextTransactionManager(TDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public void StartTransaction(TransactionalInvocationContextOptions options, InvocationContextData data)
+        {
+            _trans = _dbContext.Database.BeginTransaction(options.IsolationLevel);
+        }
+
+        public void Commit()
+        {
+            _trans!.Commit();
+        }
+
+        public void Rollback()
+        {
+            _trans!.Rollback();
+        }
+    }
+}
---
+++ src/InvocationContext.EntityFrameworkCore/ServiceCollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using InvocationContext.Transactional;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace InvocationContext.EntityFrameworkCore
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddScopedDbContextTransactionalInvocationContext<TDbContext>(this IServiceCollection sc) where TDbContext : DbContext
+        {
+            sc.AddScopedTransactionalInvocationContext();
+
+            sc.AddScoped<ITransactionManager, DbContextTransactionManager<TDbContext>>();
+        }
+    }
+}
